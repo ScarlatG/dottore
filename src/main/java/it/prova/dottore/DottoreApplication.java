5ee0af66d242eb725cc95c62769cb16122290466
Gabriel Scarlat <gabrielscarlat36@gmail.com>
2023-05-29 10:47:55 +0200

    Aggiunto run DottoreApplication

--- src/main/java/it/prova/dottore/DottoreApplication.java
+++ src/main/java/it/prova/dottore/DottoreApplication.java
@@ -1,13 +1,20 @@
 package it.prova.dottore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DottoreApplication {
+public class DottoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DottoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
 	}
 
 }
